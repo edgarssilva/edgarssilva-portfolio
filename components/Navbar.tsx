8537809050dfec363de79cfa3917bdb7f12c2a84
Edgar Silva <edgarasdaa10@gmail.com>
2022-07-28 13:03:13 +0100

    Remake navbar and headline

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,56 @@
-import { Flex, Heading, HStack, Link, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  IconButton,
+  Spacer,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
+import { FiMenu } from "react-icons/fi";
 
 const links = [
   { href: "/", label: "Projects" },
   { href: "/blog", label: "Blog" },
-  { href: "/about", label: "About Me" },
+  { href: "/about", label: "About" },
   { href: "/contact", label: "Contact" },
   { href: "/curriculum", label: "Curriculum" },
 ];
 
 export default function Navbar() {
+  const isDesktop = useBreakpointValue({ base: false, lg: true });
+
   return (
-    <Flex
-      as="header"
-      direction={"row"}
-      wrap={"nowrap"}
-      py={4}
-      px={6}
-      bg="gray.100"
-      minWidth="max-content"
-      alignItems="center"
-    >
-      <Heading>Edgar Silva</Heading>
-      <Spacer />
-      <HStack spacing={6}>
-        {links.map((l, i) => (
-          <NextLink href={l.href} key={i}>
-            <Link fontWeight={"medium"}>{l.label}</Link>
-          </NextLink>
-        ))}
-      </HStack>
-    </Flex>
+    <Box as="section" pb={{ base: "6", md: "12" }}>
+      <Box as="nav" bg="bg-surface">
+        <Container maxW="container.xl" py={{ base: "4", lg: "5" }}>
+          <HStack spacing="10" justify="space-between">
+            <Heading>Edgar Silva</Heading>
+            <Spacer />
+            {isDesktop ? (
+              <Flex justify="space-evenly" flex="1">
+                <ButtonGroup variant="link" spacing="8">
+                  {links.map((l, i) => (
+                    <NextLink href={l.href} key={i}>
+                      <Button key={i}>{l.label}</Button>
+                    </NextLink>
+                  ))}
+                </ButtonGroup>
+              </Flex>
+            ) : (
+              <IconButton
+                variant="ghost"
+                icon={<FiMenu fontSize="1.25rem" />}
+                aria-label="Open Menu"
+              />
+            )}
+          </HStack>
+        </Container>
+      </Box>
+    </Box>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import {
+  Box,
   Button,
-  ButtonGroup,
+  Container,
   Grid,
   GridItem,
   Heading,
+  Stack,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Lottie from "react-lottie-player";
@@ -17,20 +20,45 @@ const Home: NextPage = () => {
       <Navbar />
       <Grid templateColumns={[null, null, "repeat(2,1fr)"]} gap={8}>
         <GridItem my="auto">
-          <Heading mb={4} size={"3xl"}>
-            Hi{/* 👋 */}, I{"'"}m Edgar
-          </Heading>
-          <Text fontSize={"2xl"} mb={4}>
-            Computer Engineer Gradute from Aveiro, Portugal. I like to work on
-            game and web development as a full-stack.
-          </Text>
-          <ButtonGroup colorScheme="blue">
-            <Button variant="solid">View Projects</Button>
-            <Button variant="outline">Curriculum</Button>
-          </ButtonGroup>
+          <Box as="section" bg="bg-surface">
+            <Container py={{ base: "16", md: "24" }}>
+              <Stack spacing={{ base: "8", md: "10" }}>
+                <Stack spacing={{ base: "4", md: "5" }} /*  align="center" */>
+                  <Heading
+                    size={useBreakpointValue({ base: "2xl", md: "2xl" })}
+                  >
+                    Hi{/* 👋 */}, I{"'"}m Edgar
+                  </Heading>
+                  <Text
+                    color="muted"
+                    maxW="2xl"
+                    // textAlign="center"
+                    fontSize="xl"
+                  >
+                    Computer Engineer Gradute from Aveiro, Portugal. I like to
+                    work on game and web development as a full-stack.
+                  </Text>
+                </Stack>
+                <Stack
+                  spacing="3"
+                  direction={{ base: "column", sm: "row" }}
+                  // justify="center"
+                >
+                  <Button variant="outline" colorScheme="teal" size="lg">
+                    Curriculum
+                  </Button>
+                  <Button variant="solid" colorScheme="teal" size="lg">
+                    My Projects
+                  </Button>
+                </Stack>
+              </Stack>
+            </Container>
+          </Box>
         </GridItem>
-        <GridItem>
-          <Lottie loop animationData={lottieJson} play speed={0.5} />
+        <GridItem my="auto">
+          <Container>
+            <Lottie loop animationData={lottieJson} play speed={0.5} />
+          </Container>
         </GridItem>
       </Grid>
     </>
