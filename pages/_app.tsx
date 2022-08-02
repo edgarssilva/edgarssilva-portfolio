9feb6a78132553d1a69465ab4c7e4c4c27890a48
Edgar Silva <edgarasdaa10@gmail.com>
2022-08-02 15:36:28 +0100

    Theme Refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import theme from "../theme";
+import Theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <Theme>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </Theme>
   );
 }
 
---
+++ theme/index.tsx
@@ -0,0 +1,34 @@
+import {
+  ChakraProvider,
+  extendTheme,
+  ThemeOverride,
+  type ThemeConfig,
+} from "@chakra-ui/react";
+import { ThemeProvider } from "@emotion/react";
+import { PropsWithChildren } from "react";
+
+import Fonts, { fonts } from "./fonts";
+
+const config: ThemeConfig = {
+  initialColorMode: "system",
+  useSystemColorMode: false,
+};
+
+const overrides: ThemeOverride = {
+  fonts,
+  config,
+};
+
+const theme = extendTheme(overrides);
+
+const Theme: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <ThemeProvider theme={theme}>
+        <Fonts />
+        {children}
+      </ThemeProvider>
+    </ChakraProvider>
+  );
+};
+export default Theme;
